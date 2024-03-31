a4d72294930c3d315e2aa6e68815ff7f3218fa74
Alysson Lopes <nexus.af@gmail.com>
2024-03-30 22:34:24 -0400

    todoListController refactor

--- src/controllers/todoController.js
+++ src/controllers/todoController.js
@@ -1,5 +1,6 @@
 import TodoList from '../models/todoList.js';
 import  isValidTask  from '../../validators/validation.js';
+
 const todoList = new TodoList();
 
 function getAllTodos(req, res) {
@@ -9,23 +10,33 @@ function getAllTodos(req, res) {
 function addTodo(req, res) {
     const { task } = req.body;
 
-    if(isValidTask(task)) {
-        const newTodo = todoList.addTodo(task);
-        res.status(201).json(newTodo);
-    } else {
+    if(!isValidTask(task)) {
         res.status(400).json({ error: 'Task must be defined and not empty' });
     }
+
+    const newTodo = todoList.addTodo(task);
+    res.status(201).json(newTodo);
 }
 
 function updateTodo(req, res) {
     const { id } = req.params;
     const { task, completed } = req.body;
+
+    if(!isValidTask(task)) {
+        res.status(400).json({ error: 'Task must be defined and not empty' });
+    }
+
+    if(typeof completed !== 'boolean') {
+        res.status(400).json({ error: 'Completed must be a boolean' });
+    }
+
     const updatedTodo = todoList.updateTodo(id, task, completed);
-    if(updatedTodo) {
-        res.status(200).json(updatedTodo);
-    } else {
+
+    if(!updatedTodo) {
         res.status(404).json({ message: 'Todo not found' });
     }
+
+    res.status(200).json(updatedTodo);
 }
 
 function deleteTodo(req, res) {
