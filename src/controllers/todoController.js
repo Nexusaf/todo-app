183322281c9e4912bc7aa671c4afcadf261b6f94
Alysson Lopes <nexus.af@gmail.com>
2024-04-02 00:30:33 -0400

    Render front-end filled with data

--- src/controllers/todoController.js
+++ src/controllers/todoController.js
@@ -4,23 +4,40 @@ import  isValidTask  from '../../validators/validation.js';
 const todoList = new TodoList();
 
 function getAllTodos(req, res) {
-    res.status(200).json(todoList.getAllTodos())
+    const data = todoList.getAllTodos();
+    const isTest = req.body.test;
+    if(!data) {
+        res.status(404).json({ message: 'Todo list is empty' });
+    }
+
+    if(isTest) {
+        res.status(200).json(data);
+    }
+    
+    res.render('todos', { tasks: data });
 }
 
 function addTodo(req, res) {
     const { task } = req.body;
+    const isTest = req.body.test;
 
     if(!isValidTask(task)) {
         res.status(400).json({ error: 'Task must be defined and not empty' });
     }
 
     const newTodo = todoList.addTodo(task);
-    res.status(201).json(newTodo);
+
+    if(isTest) {
+        res.status(201).json(newTodo);
+    }
+
+    getAllTodos(req, res);
 }
 
 function updateTodo(req, res) {
     const { id } = req.params;
-    const { task, completed } = req.body;
+    const { task, completed, test } = req.body;
+    const isTest = test;
 
     if(!isValidTask(task)) {
         res.status(400).json({ error: 'Task must be defined and not empty' });
@@ -36,13 +53,24 @@ function updateTodo(req, res) {
         res.status(404).json({ message: 'Todo not found' });
     }
 
-    res.status(200).json(updatedTodo);
+    if(isTest) {
+        res.status(200).json(updatedTodo);
+    }
+    
+    /* Elaborate the mechanism for update in front-end
+    getAllTodos(req, res); depends on ui in front-end due to performance reasons */
 }
 
 function deleteTodo(req, res) {
     const { id } = req.params;
-    todoList.removeTodo(id);
-    res.sendStatus(204);
+    const isTest = req.body.test;
+
+    if(isTest) {
+        todoList.removeTodo(id);
+        res.sendStatus(204);
+    }
+    
+    getAllTodos(req, res);
 }
 
 export {
