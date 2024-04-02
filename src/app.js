71012c9449528989b541b349640be19a907e9742
Alysson Lopes <nexus.af@gmail.com>
2024-04-02 03:01:23 -0400

    Loggin tools added

--- src/app.js
+++ src/app.js
@@ -2,6 +2,7 @@ import express from 'express';
 import bodyParser from 'body-parser';
 import router from './routes/todoRoutes.js';
 import path from 'path';
+import morgan from'morgan';
 
 let pathToViews = path.join(path.resolve(), 'views');
 
@@ -10,7 +11,8 @@ const app = express();
 app.set('views', pathToViews);
 app.set('view engine', 'pug');
 
-app.use(express.static('public'))
+app.use(morgan('dev'));
+app.use(express.static('public', { maxAge: '1d' }));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(router);
