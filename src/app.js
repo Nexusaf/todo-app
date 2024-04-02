6e9aed52aa5861a753a48f37b9868e6dbfa06c79
Alysson Lopes <nexus.af@gmail.com>
2024-04-02 00:28:39 -0400

    Front end settings

--- src/app.js
+++ src/app.js
@@ -1,11 +1,16 @@
 import express from 'express';
 import bodyParser from 'body-parser';
 import router from './routes/todoRoutes.js';
+import path from 'path';
+
+let pathToViews = path.join(path.resolve(), 'views');
 
 const app = express();
 
+app.set('views', pathToViews);
 app.set('view engine', 'pug');
 
+app.use(express.static('public'))
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(router);
