24892a2c0ba1bbf9f929e92d6f17755a4be963d4
Alysson Lopes <nexus.af@gmail.com>
2024-04-02 00:34:06 -0400

    Differ testing enviroment after rendering implementation

--- tests/integration/api.test.js
+++ tests/integration/api.test.js
@@ -4,7 +4,7 @@ import app from '../../src/app.js';
 describe('API Endpoints', () => {
     describe('GET /todos', () => {
         test('Should return all todos', async () => {
-            const response = await request(app).get('/todos');
+            const response = await request(app).get('/todos').send({test: true});
 
             expect(response.statusCode).toBe(200);
             expect(Array.isArray(response.body)).toBe(true);
@@ -16,7 +16,7 @@ describe('API Endpoints', () => {
     describe('POST /todos', () => {
         test('Should add a new todo', async () => {
             const newTodo = { task: 'New todo' };
-            const response = await request(app).post('/todos').send(newTodo);
+            const response = await request(app).post('/todos').send(newTodo).send({test: true});
 
             expect(response.status).toBe(201);
             expect(response.body).toHaveProperty('id');
@@ -30,7 +30,7 @@ describe('API Endpoints', () => {
     describe('PUT /todos/:id', () => {
         test('Should update a todo', async () => {
             const updatedTodo = { task: 'Updated todo', completed: true };
-            const updatedResponse = await request(app).put(`/todos/${testId}`).send(updatedTodo);
+            const updatedResponse = await request(app).put(`/todos/${testId}`).send(updatedTodo).send({test: true});
 
             expect(updatedResponse.status).toBe(200);
             expect(updatedResponse.body).toHaveProperty('id');
@@ -41,11 +41,11 @@ describe('API Endpoints', () => {
 
     describe('DELETE /todos/:id', () => {
         test('Should delete a todo', async () => {
-            const response = await request(app).delete(`/todos/${testId}`);
+            const response = await request(app).delete(`/todos/${testId}`).send({test: true});
 
             expect(response.statusCode).toBe(204);
-            const allTodos = await request(app).get('/todos');
-            const result = allTodos.body.find(todo => todo.id == testId)
+            const allTodos = await request(app).get('/todos').send({test: true});
+            const result = allTodos.body.find(todo => todo.id == testId);
             expect(result).toBe(undefined);
         });
     });
